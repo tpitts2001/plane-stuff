413c054cf0dfdf38c32f3e783ca47031f2d5c86c
Taylor <fishtastic25@gmail.com>
2024-01-04 12:53:05 -0500

    i dont know what im doing

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
- 
+import requests
+import json
+import sys
+import varibles
+
+sys.path.append(r'C:\Users\fisht\PycharmProjects\plane stuff')
+
+def get_api_key():
+    # Retrieve the API key from varibles.py
+    return varibles.access_key
+
+def get_global_airlines(api_key):
+    url = "http://api.aviationstack.com/v1/airlines"
+    params = {
+        'access_key': api_key
+    }
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        return json.loads(response.text)['data']
+    else:
+        print(f"Error: {response.status_code}")
+        return None
+
+
+# Usage
+api_key = get_api_key()
+if api_key:
+    airlines = get_global_airlines(api_key)
+    if airlines:
+        for airline in airlines:
+            print(airline['airline_name'], airline['iata_code'])
--- varibles.py
+++ varibles.py
@@ -1,1 +1,1 @@
-api_key = "4fe4500b6fd8b45c547faa198e9fdb45"
+access_key = "4fe4500b6fd8b45c547faa198e9fdb45"
